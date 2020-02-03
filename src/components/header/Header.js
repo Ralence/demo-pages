4fee578d983de4b547f174eb3f3b75cee73dd1d5
Ranko Ostojic <ranko.ostojic@gmail.com>
2020-02-03 15:27:50 +0100

    feat: add search field and add material icons package

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,48 +1,105 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Typography from '@material-ui/core/Typography';
-import logo from '../../assets/logo.png';
-
+import React from "react";
+import { makeStyles, fade } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
+import logo from "../../assets/logo.png";
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1,
-    },
-    header: {
-        flexGrow: 1,
-        height: '70px',
-        backgroundColor: 'white',
-        fontFamily: 'roboto',
-        color: '#293059',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between'
-    },
-    img: {
-        height: '60px',
-        width: '150px',
-        margin: "5px",
-        marginLeft: '20px'
-    },
-    title: {
-        display: 'inline-block',
-        marginRight: '20px'
+  root: {
+    flexGrow: 1
+  },
+  header: {
+    flexGrow: 1,
+    height: "120px",
+    backgroundColor: "white",
+    fontFamily: "roboto",
+    color: "#293059",
+    flexDirection: "column",
+    alignItems: "felx-start",
+    justifyContent: "space-between",
+    [theme.breakpoints.up("sm")]: {
+        flexDirection: "row",
+        alignItems: "center",
+        height: "70px"
     }
+  },
+  img: {
+    height: "60px",
+    width: "150px",
+    margin: "5px",
+    marginLeft: "20px"
+  },
+  title: {
+    display: "inline-block",
+    margin: "auto"
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    margin: "5px",
+    marginLeft: "20px",
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      margin: theme.spacing(1),
+      width: "auto"
+    }
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  inputRoot: {
+    color: "inherit",
+    backgroundColor: "#cccccc21"
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: 120,
+      "&:focus": {
+        width: 200
+      }
+    }
+  }
 }));
 
 export default function ButtonAppBar() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>
-            <AppBar elevation={1} className={classes.header} position="static">
-                <img className={classes.img} src={logo} alt='gigaaa logo' />
-
-                <Typography variant="h6" className={classes.title}>
-                    Demo pages
-          </Typography>
-            </AppBar>
-        </div>
-    );
+  return (
+    <div className={classes.root}>
+      <AppBar elevation={1} className={classes.header} position="static">
+        <img className={classes.img} src={logo} alt="gigaaa logo" />
+          {/*<Typography variant="h6" className={classes.title}>
+            Demo pages
+  </Typography>*/}
+          <div className={classes.search} elevation={1}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
+      </AppBar>
+    </div>
+  );
 }
