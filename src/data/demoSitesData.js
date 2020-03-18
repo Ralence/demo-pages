8ee457c11e5f4a3a2030f5916f51c40068458df0
Ranko Ostojic <ranko.ostojic@gmail.com>
2020-03-18 14:55:52 +0100

    feat: add dusseldorf corona page

--- src/data/demoSitesData.js
+++ src/data/demoSitesData.js
@@ -23,6 +23,7 @@ import hibboux from "../assets/hibboux.png";
 import neuhaus from "../assets/neuhaus.png";
 import das_landesportal from "../assets/das_landesportal.png";
 import bundesministerium from "../assets/bundesministerium.png";
+import dusseldorf_corona from "../assets/dusseldorf_corona.png";
 
 class demo {
   constructor(title, img, url) {
@@ -57,7 +58,8 @@ const demos = [
   new demo("Hibboux", hibboux, "https://plugins.gigaaa.com/hibboux/"),
   new demo("Paul-Neuhaus", neuhaus, "https://plugins.gigaaa.com/paul-neuhaus/"),
   new demo("Das Landesportal", das_landesportal, "https://plugins.gigaaa.com/das_landesportal/"),
-  new demo("Bundesministerium", bundesministerium, "https://plugins.gigaaa.com/bundesministerium/")
+  new demo("Bundesministerium", bundesministerium, "https://plugins.gigaaa.com/bundesministerium/"),
+  new demo("Dusseldorf Corona", dusseldorf_corona, "https://plugins.gigaaa.com/duseldorf_corona/")
 ];
 
 export default demos;
